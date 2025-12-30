99aed568ee5f0248fb9e3ccd5eb775db0f3e6c8d
Maria Carolina <mcarolinafll@gmail.com>
2025-12-30 15:55:14 -0300

    Delete Carol directory

--- Carol/atualizar.php
+++
@@ -1,6 +0,0 @@
-<?php
-// Este arquivo pode ser removido, a funcionalidade de atualizar está em editar_candidato.php
-header('Location: dashboard.php');
-exit;
-?>
-
--- Carol/atualizar2.php
+++
@@ -1,6 +0,0 @@
-<?php
-// Este arquivo pode ser removido, a funcionalidade de atualizar está em editar_empresa.php
-header('Location: dashboard2.php');
-exit;
-?>
-
